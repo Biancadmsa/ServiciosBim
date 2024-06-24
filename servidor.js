e227c3d29a7067359afc3bb04ede534367b24ab0
biancadmsadf16@gmail.com <biancadmsadf16@gmail.com>
2024-06-24 12:38:58 -0400

    primer commit

--- servidor.js
+++ servidor.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-const port = 3001;
+const port = 3000;
 const app = express();
 
 app.use(express.json());
