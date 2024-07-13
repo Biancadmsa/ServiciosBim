ddd701be5d25c8a4763fce2214695cf6da50bdbe
biancadmsadf16@gmail.com <biancadmsadf16@gmail.com>
2024-07-13 19:15:08 -0400

    Mover index.html a la raíz

--- servidor.js
+++ servidor.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const transporter = nodemailer.createTransport({
